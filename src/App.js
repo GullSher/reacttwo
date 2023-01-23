e621afc23616d226f63bdbb3c4e25aa432ec2776
Gull-Sher <mobilegull73@gmail.com>
2023-01-23 22:38:40 +0500

    according to thapa, to create a MenuBar I have to install react-rounter-dom.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-// import Header from './Assets/Components/Header.jsx';
 import MenuBar from './Assets/Components/MenuBar.jsx';
-
+import Header from './Assets/Components/Header.jsx';
 // import Header1 from './Assets/Components/Header1.jsx';
 import Header2 from './Assets/Components/Header2.jsx';
-
+import Footer from './Assets/Components/Footer';
+import Footer2 from './Assets/Components/Footer2';
 
 
 function App() {
   return (
     <div className="App">
       <div><h2>This is going to recreate just to update git.</h2></div>
-      {/* <Header /> */}
+      <MenuBar />
+      <Header />
       {/* <Header1 /> */}
       <Header2 />
-      <MenuBar />
+      <Footer />
+      <Footer2 />
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
---
+++ src/Assets/Components/PageTwo.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function PageTwo() {
+    return (
+        <div>PageTwo</div>
+
+    )
+}
